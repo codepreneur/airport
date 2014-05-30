3a825e40754ec57bc3a9efc7a04124bedf260ed2
gomenasaai <contramundee@gmail.com>
2014-05-30 11:50:56 +0100

    added plane with its corresponding unit tests

---
+++ lib/plane.rb
@@ -0,0 +1,22 @@
+class Plane
+
+	def initialize(status='flying')
+		@status = status
+	end
+
+	def status?
+		@status
+	end
+
+	def take_off
+		@status = 'flying'
+		self
+	end
+
+	def land
+		@status = 'landed'
+		self
+	end
+
+
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,33 @@
+require 'plane'
+
+# When we create a new plane, it should have a "flying" status, thus planes can not be created in the airport.
+#
+# When we land a plane at the airport, the plane in question should have its status changed to "landed"
+#
+# When the plane takes of from the airport, the plane's status should become "flying"
+
+describe Plane do
+
+	let(:plane) {Plane.new}
+	
+	it 'has a flying status when created' do
+		expect(plane.status?).to eq 'flying'
+	end
+
+	it 'has a flying status when in the air' do
+		plane.take_off
+		expect(plane.status?).to eq 'flying'
+	end
+
+	it 'can take off' do
+		expect(plane).to receive(:take_off)
+		plane.take_off
+	end
+
+	it 'changes its status to flying after taking off' do
+		plane.land.take_off
+		expect(plane.status?).to eq 'flying'
+	end
+
+
+end
