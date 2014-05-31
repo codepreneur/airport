ede86f52fe82e12050a761cbb0a80c0574e8d4c8
gomenasaai <contramundee@gmail.com>
2014-06-01 00:58:53 +0100

    upgraded controller engine

--- lib/controller.rb
+++ lib/controller.rb
@@ -6,12 +6,16 @@ class Controller
 
 
 	def send_plane(from_airport,plane,sky,to_airport)
-		from_airport.release_a(plane)
+		until from_airport.check == 'sunny' do
+			from_airport.release_a(plane)
+		end
 		plane.take_off!
 		sky.add(plane)
-		to_airport.land_a(plane)
+		until to_airport.check == 'sunny' && !to_airport.full? do
+			to_airport.land_a(plane)
+		end
 		plane.land!
-		
+
 	end
 
 	def add_plane_to(airport,plane)
