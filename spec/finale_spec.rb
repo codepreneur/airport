fbc1cc770d5d5d75e10276f4f6d69210f2c39695
gomenasaai <contramundee@gmail.com>
2014-06-04 22:44:30 +0100

    started finale

--- spec/finale_spec.rb
+++ spec/finale_spec.rb
@@ -4,7 +4,8 @@
 # Be careful of the weather, it could be stormy!
 # Check when all the planes have landed that they have the right status "landed"
 # Once all the planes are in the air again, check that they have the status of flying!
+
 describe "The gand finale (last spec)" do
-  xit 'all planes can land and all planes can take off' do
+  it 'all planes can land and all planes can take off' do
   end
 end
